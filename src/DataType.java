3a532fc5ff6e9c23a388f9b4077df9ef8ebc7391
wiryamanto <wiryamantoragil@gmail.com>
2023-10-12 20:33:25 +0700

    1- Learning Data Type

---
+++ src/DataType.java
@@ -0,0 +1,54 @@
+public class DataType {
+    public static void main(String[] args){
+//        Tipe data Primitive :
+//        Integer, Boolean, Char, Byte, Float, BigInterger, Double
+//        Contoh:
+
+//        Tipe data Integer:
+        int angka1 = 4;
+        int angka2 = 8;
+        int angka3 = 10;
+        int umur = 18;
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+
+//        Tipe Data Boolean:
+        boolean pilih = false;
+        boolean maju = true;
+        System.out.println(pilih);
+        System.out.println(maju);
+
+//        Tipe Data Char (Karakter) / 1 Huruf
+        char inisial_nama = 'R';
+        char kode = 'E';
+        char Jenis_kelamin = 'L';
+        System.out.println(inisial_nama);
+        System.out.println(kode);
+        System.out.println(Jenis_kelamin);
+
+//        Tipe Data Byte:
+        byte number1 = 0;
+        byte number2 = 6;
+//        byte kilometer = 247; // jika ingin menggunakan tipe data byte seharusnya hanya menyimpang angka dalam kisaran -128 sampai + 127
+        byte number3 = 56;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+
+//        Tipe data Float:
+        float berat_badan = 58.7f;
+        float tonase = 7.0f;
+        float nilai_matematika = 59f;
+        System.out.println(berat_badan);
+        System.out.println(tonase);
+        System.out.println(nilai_matematika);
+
+//        Tipe Data Double
+        double waktu_tempuh = 4.5;
+        double nilai_Ipa = 89.8;
+        System.out.println(waktu_tempuh);
+        System.out.println(nilai_Ipa);
+    }
+}
---
+++ src/Latihan1.java
@@ -0,0 +1,21 @@
+public class Latihan1 {
+
+    public static void main (String[] args){
+//        System.out.println("Hello program pertama saya");
+//        1. Definisi variable
+        int angka1;
+        int angka2;
+        int hasil_pengurangan;
+
+//        2. Inputkan value ke variable
+        angka1 = 20;
+        angka2 = 4;
+
+//        3. Proses perhitungan
+        hasil_pengurangan = angka1 - angka2;
+
+//        4. output
+        System.out.println("jadi hasilnya adalah " + hasil_pengurangan);
+    }
+
+}
